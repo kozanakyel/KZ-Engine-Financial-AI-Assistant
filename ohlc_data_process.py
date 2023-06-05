d9eb737bb0304531a7e50e10d3e39927a5bdc79a
kozanakyel <kozanakyel@gmail.com>
2023-06-06 00:04:43 +0300

    add indicator calculation

---
+++ ohlc_data_process.py
@@ -0,0 +1,25 @@
+import yfinance as yf
+import pandas as pd
+import pandas_ta as ta
+
+
+def fetch_data(symbol: str, period: str, interval: str):
+    # Fetch Bitcoin data from Yahoo Finance
+    ohlc_data = yf.download(tickers=symbol, period=period, interval=interval, progress=False)
+    return ohlc_data
+
+
+def calculate_dmi_rsi_mfi(data):
+    data.ta.adx(length=14, append=True)
+    data.ta.rsi(length=14, append=True)
+    data.ta.mfi(length=14, append=True)
+    data = data.dropna(axis=0)
+    return data
+
+
+data = fetch_data('BTC-USD', '3mo', '1d')
+indicator_data = calculate_dmi_rsi_mfi(data)
+print(indicator_data.DMP_14.iloc[-1])
+
+
+
