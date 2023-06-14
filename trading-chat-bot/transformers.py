f62533dab78bbf255cd48dbd89fca28855e67559
kozanakyel <kozanakyel@gmail.com>
2023-06-14 14:09:42 +0300

    added new confgis

--- trading-chat-bot/transformers.py
+++ trading-chat-bot/transformers.py
@@ -7,6 +7,7 @@ import numpy as np
 from config import TEXT_EMBEDDING_CHUNK_SIZE, EMBEDDINGS_MODEL
 from database import load_vectors
 
+
 def get_col_average_from_list_of_lists(list_of_lists):
     """Return the average of each column in a list of lists."""
     if len(list_of_lists) == 1:
@@ -15,6 +16,7 @@ def get_col_average_from_list_of_lists(list_of_lists):
         list_of_lists_array = array(list_of_lists)
         average_embedding = average(list_of_lists_array, axis=0)
         return average_embedding.tolist()
+
 
 # Create embeddings for a text using a tokenizer and an OpenAI engine
 
@@ -32,8 +34,10 @@ def create_embeddings_for_text(text, tokenizer):
 
     return (text_embeddings, average_embedding)
 
+
 def get_embeddings(text_array, engine):
     return openai.Engine(id=engine).embeddings(input=text_array)["data"]
+
 
 # Split a text into smaller chunks of size n, preferably ending at the end of a sentence
 def chunks(text, n, tokenizer):
@@ -54,9 +58,11 @@ def chunks(text, n, tokenizer):
             j = min(i + n, len(tokens))
         yield tokens[i:j]
         i = j
-        
+
+
 def get_unique_id_for_file_chunk(filename, chunk_index):
-    return str(filename+"-!"+str(chunk_index))
+    return str(filename + "-!" + str(chunk_index))
+
 
 def handle_file_string(file, tokenizer, redis_conn, text_embedding_field, index_name):
     """
@@ -81,7 +87,7 @@ def handle_file_string(file, tokenizer, redis_conn, text_embedding_field, index_
 
     # Clean up the file string by replacing newlines, double spaces, and semi-colons
     clean_file_body_string = file_body_string.replace("  ", " ").replace("\n", "; ").replace(';', ' ')
-    
+
     # Add the filename to the text to embed
     text_to_embed = "Filename is: {}; {}".format(filename, clean_file_body_string)
 
@@ -111,11 +117,10 @@ def handle_file_string(file, tokenizer, redis_conn, text_embedding_field, index_
 
 # Make a class to generate batches for insertion
 class BatchGenerator:
-    
-    
+
     def __init__(self, batch_size: int = 10) -> None:
         self.batch_size = batch_size
-    
+
     # Makes chunks out of an input DataFrame
     def to_batches(self, df: pd.DataFrame) -> Iterator[pd.DataFrame]:
         splits = self.splits_num(df.shape[0])
@@ -128,5 +133,5 @@ class BatchGenerator:
     # Determines how many chunks DataFrame contains
     def splits_num(self, elements: int) -> int:
         return round(elements / self.batch_size)
-    
+
     __call__ = to_batches
