a005c53c1fc2b55377d62c9996f3cde41f8b1efb
kozanakyel <kozanakyel@gmail.com>
2024-02-11 20:30:42 +0100

    edited some changes

--- notebooks_chats/trading_advisor.py
+++ notebooks_chats/trading_advisor.py
@@ -1,7 +1,13 @@
 from dotenv import load_dotenv
 import os
 import matplotlib.pyplot as plt
-from ohlc_data_process import calculate_dmi_rsi_mfi, fetch_data, analyze_ichimoku, analyze_supertrend, calculate_supertrend
+from ohlc_data_process import (
+    calculate_dmi_rsi_mfi,
+    fetch_data,
+    analyze_ichimoku,
+    analyze_supertrend,
+    calculate_supertrend,
+)
 from langchain import OpenAI
 from langchain.chat_models import ChatOpenAI
 from langchain import FewShotPromptTemplate, PromptTemplate
@@ -9,37 +15,38 @@ import random
 
 load_dotenv()
 
-openai_api_key = os.getenv('T_OPENAI_API_KEY')
-os.environ['OPENAI_API_KEY'] = openai_api_key
+openai_api_key = os.getenv("T_OPENAI_API_KEY")
+os.environ["OPENAI_API_KEY"] = openai_api_key
 
 openai = OpenAI(
-    #model_name='text-davinci-003',
-    model_name='text-davinci-003',
-    temperature=0.7
+    # model_name='text-davinci-003',
+    model_name="text-davinci-003",
+    temperature=0.7,
 )
 
 
 # create our examples
 trend_examples = [
-{
-    "query": f"Ichmouku trend,Strong Bullish,Detected price,110.34,Supertrend,uptrend,"
-                f" cross for supertrend,True,Current Price,95.123"
-                f" What is your advice for trading for this trend and price values?",
-    "answer": "Our Ichmouku trend is Strong bullish that is good for next time step, we can say 110.34 our resistance \
+    {
+        "query": f"Ichmouku trend,Strong Bullish,Detected price,110.34,Supertrend,uptrend,"
+        f" cross for supertrend,True,Current Price,95.123"
+        f" What is your advice for trading for this trend and price values?",
+        "answer": "Our Ichmouku trend is Strong bullish that is good for next time step, we can say 110.34 our resistance \
         because the detected price from ichmouku is greater than our current price 95.123. Also we have \
         croos line and trend changing step for our supertrend indicator within 5 hour and the supertrend indicator momuntum is uptrend. \
             You can hold if you have this coin. Or you can buy this coin because we have trend changing for uptrend \
                 and have a strong bullish momentum from ichmouku. you can follow this holding opeartion \
-                    our detected resistance price to 110.34"
-}, {
-    "query": f"Ichmouku trend,Bearish,Detected price,90.34,Supertrend,downtrend,"
-                f" cross for supertrend,No,Current Price,95.123"
-                f" What is your advice for trading for this trend and price values?",
-    "answer": "Our ichmouku trend is bearish not string enough, detected price is 90.34 and we can say this is our \
+                    our detected resistance price to 110.34",
+    },
+    {
+        "query": f"Ichmouku trend,Bearish,Detected price,90.34,Supertrend,downtrend,"
+        f" cross for supertrend,No,Current Price,95.123"
+        f" What is your advice for trading for this trend and price values?",
+        "answer": "Our ichmouku trend is bearish not string enough, detected price is 90.34 and we can say this is our \
         supported price because current price is greater than our detected price. Also we have a downtrend for supertrend indicator. \
             we can say you can wait for buying because we dont see any cross in supertrend or bullish movement \
-                from our trade engine."
-}
+                from our trade engine.",
+    },
 ]
 
 trend_prefix = """Answer the question based on the context below.
@@ -55,27 +62,28 @@ Here are some examples:
 
 # create our examples
 rsi_examples = [
-{
-    "query": f"RSI,70.34,MFI,59.02,DMP,31.3,"
-                f" DMN,12.77,ADX,41.26"
-                f" What is your advice for trading for those indicator values?",
-    "answer": "The RSI indicator value being above 70 at this moment indicates an overbought zone has been entered."
-                "The MFI value trending above the average confirms the flow of money. On the DMi side, "
-                "the DMP (positive directional movement indicator) value is above "
-                "the DMN (negative directional movement indicator) value, "
-                "and ADX is strongly trending above 25 and at 40 levels, "
-                "indicating a strong bull trend that has entered in a short period of time. "
-                "When considering the flow of money and the overbought zone, it may be advisable "
-                "to take some profits and waiting next market movements."
-}, {
-    "query": f"RSI,40.14, MFI,41, DMP,21.01,"
-                f"DMN,23.67,ADX,20.76."
-                f" What is your advice for trading for those indicator values?",
-    "answer": "The RSI indicator value dropping around 40 indicates approaching the selling zone. "
-                "The MFI index also dropping around 40 supports this. Although ADX suggests that there is no strong trend below 25, "
-                "it can be observed that DMN is above DMP, creating selling pressure. "
-                "My recommendation would be to wait for a better buying opportunity at this point."
-}
+    {
+        "query": f"RSI,70.34,MFI,59.02,DMP,31.3,"
+        f" DMN,12.77,ADX,41.26"
+        f" What is your advice for trading for those indicator values?",
+        "answer": "The RSI indicator value being above 70 at this moment indicates an overbought zone has been entered."
+        "The MFI value trending above the average confirms the flow of money. On the DMi side, "
+        "the DMP (positive directional movement indicator) value is above "
+        "the DMN (negative directional movement indicator) value, "
+        "and ADX is strongly trending above 25 and at 40 levels, "
+        "indicating a strong bull trend that has entered in a short period of time. "
+        "When considering the flow of money and the overbought zone, it may be advisable "
+        "to take some profits and waiting next market movements.",
+    },
+    {
+        "query": f"RSI,40.14, MFI,41, DMP,21.01,"
+        f"DMN,23.67,ADX,20.76."
+        f" What is your advice for trading for those indicator values?",
+        "answer": "The RSI indicator value dropping around 40 indicates approaching the selling zone. "
+        "The MFI index also dropping around 40 supports this. Although ADX suggests that there is no strong trend below 25, "
+        "it can be observed that DMN is above DMP, creating selling pressure. "
+        "My recommendation would be to wait for a better buying opportunity at this point.",
+    },
 ]
 
 # create a example template
@@ -86,8 +94,7 @@ AI: {answer}
 
 # create a prompt example from above template
 example_prompt = PromptTemplate(
-    input_variables=["query", "answer"],
-    template=example_template
+    input_variables=["query", "answer"], template=example_template
 )
 
 # now break our previous prompt into a prefix and suffix
@@ -119,7 +126,7 @@ AI: """
 
 
 class TradingAdvisor:
-    
+
     @staticmethod
     def create_advice_prompt_template(examples, prefix):
         few_shot_prompt_template = FewShotPromptTemplate(
@@ -128,16 +135,16 @@ class TradingAdvisor:
             prefix=prefix,
             suffix=suffix,
             input_variables=["query"],
-            example_separator="\n\n"
+            example_separator="\n\n",
         )
         return few_shot_prompt_template
 
     @staticmethod
     def fetch_current_data(symbol: str):
-        df = fetch_data(symbol, '1mo', '1h')
+        df = fetch_data(symbol, "1mo", "1h")
         indicator_data = calculate_dmi_rsi_mfi(df)
         return indicator_data
-    
+
     @staticmethod
     def get_advice(symbol: str):
         df = TradingAdvisor.fetch_current_data(symbol)
@@ -146,40 +153,41 @@ class TradingAdvisor:
         dmp_14 = df.DMP_14.iloc[-1]
         dmn_14 = df.DMN_14.iloc[-1]
         adx_14 = df.ADX_14.iloc[-1]
-        
+
         trend_ich, base_price = analyze_ichimoku(df)
         spr_trend = calculate_supertrend(df)
-        trend_super, cross_super = analyze_supertrend(spr_trend) 
-        
-        # print(trend_ich, base_price, trend_super, cross_super, df.iloc[-1]["Close"])
-        
-        rsi_query = f"RSI,{rsi_14:.2f},MFI,{mfi_14:.2f},DMP,{dmp_14:.2f}," \
-                f" DMN,{dmn_14:.2f},ADX,{adx_14:.2f}"     
-                
-        trend_query = f"Ichmouku trend,{trend_ich},Detected price,{base_price[0]:.2f},Supertrend,{trend_super}," \
-                f" cross for supertrend,{cross_super},Current Price,{df.iloc[-1]['Close']:.2f}"   
+        trend_super, cross_super = analyze_supertrend(spr_trend)
 
-        rsi_prompt_template = TradingAdvisor.create_advice_prompt_template(rsi_examples, rsi_prefix)
-        trend_prompt_template = TradingAdvisor.create_advice_prompt_template(trend_examples, trend_prefix)
+        # print(trend_ich, base_price, trend_super, cross_super, df.iloc[-1]["Close"])
+
+        rsi_query = (
+            f"RSI,{rsi_14:.2f},MFI,{mfi_14:.2f},DMP,{dmp_14:.2f},"
+            f" DMN,{dmn_14:.2f},ADX,{adx_14:.2f}"
+        )
+
+        trend_query = (
+            f"Ichmouku trend,{trend_ich},Detected price,{base_price[0]:.2f},Supertrend,{trend_super},"
+            f" cross for supertrend,{cross_super},Current Price,{df.iloc[-1]['Close']:.2f}"
+        )
+
+        rsi_prompt_template = TradingAdvisor.create_advice_prompt_template(
+            rsi_examples, rsi_prefix
+        )
+        trend_prompt_template = TradingAdvisor.create_advice_prompt_template(
+            trend_examples, trend_prefix
+        )
         templates = [rsi_prompt_template, trend_prompt_template]
         queries = [rsi_query, trend_query]
-        
+
         if cross_super:
             prompt_template = trend_prompt_template
-            return openai(
-                prompt_template.format(
-                    query=trend_query
-                )
-            )
+            return openai(prompt_template.format(query=trend_query))
         else:
-            rand = random.randint(0,1)
+            rand = random.randint(0, 1)
             prompt_template = templates[rand]
-            return openai(
-                    prompt_template.format(
-                        query=queries[rand]
-                    )
-                )
-        
-if __name__ == '__main__':
+            return openai(prompt_template.format(query=queries[rand]))
+
+
+if __name__ == "__main__":
     tradv = TradingAdvisor.get_advice("SOL-USD")
     print(tradv)
